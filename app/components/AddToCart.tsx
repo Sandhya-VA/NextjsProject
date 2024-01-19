197f27d564b6289f963dcee7c440a35c46151cf3
Sandhya <sandhyav997@gmail.com>
2024-01-19 15:59:44 -0600

    addedsomefeatures

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
-       <button onClick={()=> console.log('Click')}>AddToCart</button> 
+       <button className='btn btn-primary' onClick={()=> console.log('Click')}>AddToCart</button> 
     </div>
   )
 }
